62d9a63608a9263943db2084de2d0e81ed7cf964
Richard Steinbrecht <1713474@stud.hs-mannheim.de>
2019-11-25 14:29:19 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+const app = express();
+const path=require('path');
+var fs = require('fs');
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/:userId', (req, res) => {
+    var test = req.params.userId;
+    return res.send('Received a GET HTTP method with argument ' + test);
+});
+
+app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+});
+
+app.listen(3000, () =>
+    console.log(`Example app listening on port 3000!`),
+);

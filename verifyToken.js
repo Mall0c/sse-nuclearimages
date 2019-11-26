b2f9eb4234dc92648bed0a29e41479e65760525b
Richard Steinbrecht <1713474@stud.hs-mannheim.de>
2019-11-26 13:32:42 +0100

    JWT

--- verifyToken.js
+++ verifyToken.js
@@ -3,23 +3,23 @@ const dbConfig = require('./dbConfig');
 function verifyToken(req, res, next) {
     // Express headers are auto converted to lowercase.
     let token = req.headers['x-access-token'] || req.headers['authorization'];
-    
-    // No token provided, not logged in.
-    if (typeof token == 'undefined') {
-        return res.status(200).send({ auth: false, message: 'No token provided.' });
-    }
-    
-    // Remove Bearer from string.
-    if (token.startsWith('Bearer ')) {
-        token = token.slice(7, token.length);
-    }
-    
-    jwt.verify(token, dbConfig.secret, function(err, decoded) {
-        if (err)
-            return res.status(403).send({ auth: false, message: 'Failed to authenticate token.' });
-        // if everything good, save to request for use in other routes.
-        req.username = decoded.username;
+    // Token provided. Check for correctness.
+    if (token != null) {        
+        // Remove Bearer from string.
+        if (token.startsWith('Bearer ')) {
+            token = token.slice(7, token.length);
+        }
+
+        jwt.verify(token, dbConfig.secret, function(err, decoded) {
+            if (err)
+                return res.status(403).send({ auth: false, message: 'Failed to authenticate token.' });
+            // If everything good, save to request for use in other routes.
+            req.username = decoded.username;
+            next();
+        });
+    } else {
+        // No token provided. Do nothing.
         next();
-    });
+    }
 }
 module.exports = verifyToken;

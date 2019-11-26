ac0d33d7e946be2308d5eb07c8b778ca481e8e2a
gmpbigsun <Tuncay.Yildiz@Live.De>
2019-11-26 01:19:05 +0100

    frontend stuff
    - infinite scrolling
    - dynamic loading of images
    - upload working but no feedback yet
    - frontend ui changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,56 @@
+var offset = 0;
+
+function getImages(count, offset) {
+    var xhr = new XMLHttpRequest();
+    const columnsMax = 4;
+    xhr.open("GET", "/frontpage/" + count + "/" + offset);
+    xhr.responseType = "json";
+    xhr.onload = function() {
+        var data = xhr.response;
+        // if operation is completed and successful handle highscore data client side
+        if (data !== null && xhr.readyState === 4 && xhr.status === 200) {
+            var currentColumn = 0;
+            for (let i = 0; i < data.length; i++) {
+                if (currentColumn == columnsMax)
+                    currentColumn = 0;
+                var elem = document.createElement("img");
+                elem.src = 'data:image/png;base64,' + data[i]["image"];
+                document.getElementById("column" + (currentColumn + 1)).appendChild(elem);
+                currentColumn++;
+            }
+        }
+    };
+    xhr.send();
+}
+
+getImages(20, 0);
+
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        offset = offset + 20;
+        getImages(20, offset);
+    }
+};
+
+$(document).ready(function() {
+    $("#myForm").submit(function(e) {
+        //Stops submit button from refreshing page.
+        e.preventDefault();
+
+        var form_data = new FormData(this);
+
+        $.ajax({
+            url: '/upload', //location of where you want to send image
+            dataType: 'json', // what to expect back from the PHP script, if anything
+            cache: false,
+            contentType: false,
+            processData: false,
+            data: form_data,
+            type: 'post',
+        });
+    });
+});
+
 function readURL(input) {
     if (input.files && input.files[0]) {
 
